47ea0eff2efcf68711c5339f2272f1454ab2cd2b
Valentin Thirion <valentinthirion@gmail.com>
2016-02-18 13:46:39 +0100

    Value Computation correction

--- model/stock_inventory.py
+++ model/stock_inventory.py
@@ -1,4 +1,7 @@
 from openerp import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class stock_inventory(models.Model):
     _inherit = ['stock.inventory']
@@ -8,18 +11,11 @@ class stock_inventory(models.Model):
     @api.one
     @api.onchange('order_line')
     def _compute_value(self):
-        cr = self.env.cr
-        uid = self.env.user.id
-
         value = 0
+        for line in self.line_ids:
+            value += round(float(line.value), 2)
 
-        inventory_line_obj = self.pool.get('stock.inventory.line')
-        inventory_lines = inventory_line_obj.search(cr, uid, [('inventory_id', '=', self.id)])
-        if inventory_lines:
-            for inventory_line in inventory_line_obj.browse(cr, uid, inventory_lines):
-                value += inventory_line.value
-
-        self.total_value = value
+        self.total_value = round(float(value), 2)
 
 class stock_inventory_line(models.Model):
     _inherit = ['stock.inventory.line']
@@ -29,30 +25,17 @@ class stock_inventory_line(models.Model):
 
     @api.one
     def _compute_value_for_line(self):
-        cr = self.env.cr
-        uid = self.env.user.id
+        if len(self.product_id.seller_ids) < 1: # Product has no sellers
+            self.value = round(float(self.product_id.standard_price * self.product_qty), 2)
 
-        if self.product_id.seller_id.id: # Product has sellers
-            # get supplier info
-            obj = self.pool.get('product.supplierinfo')
-            supplier_info = obj.search(cr, uid, [('product_tmpl_id', '=', self.product_id.product_tmpl_id.id)])
-            if supplier_info and supplier_info[0]:
-                right_supplier = obj.browse(cr, uid, supplier_info[0])
-                for val in obj.browse(cr, uid, supplier_info):
-                    if val.sequence < right_supplier.sequence:
-                        right_supplier = val
-                pricelist_partnerinfo_obj = self.pool.get('pricelist.partnerinfo')
-                pricelist_partnerinfos = pricelist_partnerinfo_obj.search(cr, uid, [('suppinfo_id', '=', right_supplier.id)])
-                if pricelist_partnerinfos:
-                    computed_cost_price = 0
-                    if pricelist_partnerinfo_obj.browse(cr, uid, pricelist_partnerinfos[0]).min_quantity == 0:
-                        computed_cost_price = pricelist_partnerinfo_obj.browse(cr, uid, pricelist_partnerinfos[0]).price
-                    else:
-                        computed_cost_price = pricelist_partnerinfo_obj.browse(cr, uid, pricelist_partnerinfos[0]).price / pricelist_partnerinfo_obj.browse(cr, uid, pricelist_partnerinfos[0]).min_quantity
-                    self.value = computed_cost_price * self.product_qty
-                else:
-                    self.value = self.product_id.standard_price * self.product_qty     #error
-            else:
-                self.value = self.product_id.standard_price * self.product_qty     #error
-        else:
-            self.value = self.product_id.standard_price * self.product_qty    
+        else: # has sellers
+            suppliers = self.product_id.seller_ids.sorted(key=lambda r:r.sequence)
+            right_supplier = suppliers[0]
+
+            if len(right_supplier.pricelist_ids) < 1: # No pricelist for this product
+                self.value = round(float(self.product_id.standard_price * self.product_qty), 2)
+
+            else: # Pricelist exists
+                supplier_pricelists = right_supplier.pricelist_ids.sorted(key=lambda r:r.min_quantity)
+                pricelist_min = supplier_pricelists[0]
+                self.value = round(float(pricelist_min.price * self.product_qty ), 2)
